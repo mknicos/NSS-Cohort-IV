91b196bbe0ea2ecb8abbd4a18a9cdfbf4eca98db
Matt Knicos <mknicos@gmail.com>
2014-01-16 15:55:49 -0600

    SuperCalc 2 functions added

--- 2014-01-16-jQuery.SuperCalc/public/js/main.js
+++ 2014-01-16-jQuery.SuperCalc/public/js/main.js
@@ -1,25 +1,27 @@
 //MAIN JAVASCRIPT FILE
 
+
 $(document).ready(initialize);
+
+
 
 function initialize(){
   $('.number').click(displayNumber);
 }
 
+function displayNumber(){ 
+  var display = $('#display').text();
+  var current = this.textContent; 
+  var output;
 
+  if(current === '.' && display.indexOf('.') !== -1) return;
 
-
-function displayNumber(){
-  $('answer').text(this.textContent);
+  if(display === '0' && current !== '.')
+       output = current;
+    else
+       output = display  + current; 
+  
+    $('#answer').text(output);
 }
-
-
-
-
-
-
-
-
-
 
 
