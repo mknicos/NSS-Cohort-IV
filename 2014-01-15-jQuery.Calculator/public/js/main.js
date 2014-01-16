606f7a9106b00b716ace1730355fb65ee5b83277
Matt Knicos <mknicos@gmail.com>
2014-01-15 23:40:03 -0600

    jQuery Calc 2.0, and css

--- 2014-01-15-jQuery.Calculator/public/js/main.js
+++ 2014-01-15-jQuery.Calculator/public/js/main.js
@@ -4,12 +4,15 @@ $(document).ready(initialize);
 function initialize(){
   $('#calc').click(calculate);
   $('#clear').click(clear);
+  $('#sum').click(add);
+  $('#prod').click(multiply);
+  $('#clear2').click(clear2);
 }
 
 function clear(){
   $('#num1').val('');
   $('#num2').val('');
-  $('#num2').focus();
+  $('#num1').focus();
   $('#op').val('');
   $('#result').text('');
 }
@@ -40,12 +43,43 @@ function compute(x,y,op){
   return result;
 }
 
+function add(){
+  var num1 = parseFloat($('#input1').val());
+  var num2 = parseFloat($('#input2').val());
+  var num3 = parseFloat($('#input3').val());
+  var num4 = parseFloat($('#input4').val());
+  var num5 = parseFloat($('#input5').val());
+  var sum = num1 + num2 + num3 + num4 + num5
+  
+  $('#sumAns').text(sum);
+}
+
+function multiply(){
+  var num1 = parseFloat($('#input1').val());
+  var num2 = parseFloat($('#input2').val());
+  var num3 = parseFloat($('#input3').val());
+  var num4 = parseFloat($('#input4').val());
+  var num5 = parseFloat($('#input5').val());
+  var product = num1 * num2 * num3 * num4 * num5
+  
+  $('#prodAns').text(product);
+}
+
+
+function clear2(){
+  $('#input1').val('');
+  $('#input2').val('');
+  $('#input3').val('');
+  $('#input4').val('');
+  $('#input5').val('');
+  $('#sumAns').text('');
+  $('#prodAns').text('');
+}
 
 
 
-
-
-
+// console.log($( ".inputs" ) [2]);
+//
 
 
 
