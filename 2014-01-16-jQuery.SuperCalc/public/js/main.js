af806a8aa40c2c563d081ec130603add96529213
Matt Knicos <mknicos@gmail.com>
2014-01-17 23:08:22 -0600

    SuperCalc refactor

--- 2014-01-16-jQuery.SuperCalc/public/js/main.js
+++ 2014-01-16-jQuery.SuperCalc/public/js/main.js
@@ -49,21 +49,22 @@ function pushToQueue(){
   $li.text(display);
   $('#queue').prepend($li);
 
-  if($('li').length > 2){
-    // $('.grey-out').css('background-color', 'grey');
+  if($('li').length > 2){                  //change apperance of binary operator buttons when they cant be used
     $('.grey-out').addClass('disengage');
 
   }
 }
 
 function compute(){
-  if($('li').length > 2){
+  if($('li').length > 2){     //disallow use of binary operators when there are more than 2 #s in queue
     return;
   }
 
   var operator = this.id;
   var $lis = $('#queue li');
   var numbers = parseTags($lis);
+  if(numbers[0] == 'NaN' || numbers[1] == 'NaN') //still need to work on this
+    return;
 
   switch(operator){
    case 'add':
@@ -87,12 +88,11 @@ function compute(){
      displayAndClear(quotient);
      break;
      
-   case 'sum':
-     $('#queue li').each(function(index, value){
-       x = parseFloat(value)
+   case 'sum':                    //need to move sum and pow out of compute function
+      alert('this is working');
+      $('#display').text('Hey');
+     
 
-      $('#display').text(value);
-     })
      break;
 
    case 'pow':
@@ -103,12 +103,12 @@ function compute(){
 }
 
 
-function displayAndClear(x){
+function displayAndClear(x){          //will display the argument to display and clear queue
   $('#display').text(x);
   $('#queue').text('');
 }
 
-function clearEverything(){
+function clearEverything(){           //Clears display, clears queue, and returns buttons to normal state
   $('#display').text('0');
   $('#queue').text(''); 
   $('.disengage').removeClass('disengage');
@@ -118,12 +118,6 @@ function clearEverything(){
 
 
 
- /* 
-     $.each($('#queue li'),function(index, value) {
-     var  ans = ans + value;
-    $('#display').text(ans);
-      })
- */
 
 /*
 function flipSign(){
