b99ef23e92abe3d01ec4cafd88a511fb3342f780
Matt Knicos <mknicos@gmail.com>
2014-01-27 14:47:11 -0600

    Array's modified with LoDash

---
+++ 2014-01-27-Lo-Dash.Demo/app/js/source/main.js
@@ -0,0 +1,39 @@
+(function(){
+
+  'use strict';
+
+  $(document).ready(init);
+
+  function init(){
+    $('#go').click(go);
+
+  }
+
+  function go(){
+    var data = $('#data').val();
+    data = data.split(';');
+    var start = data[0];
+    var stop = data[1];
+    var increment = data[2];
+
+    var array = _.range(start, stop, increment);
+    var next = _.filter(array, function(x){
+      return((x%3 === 0) && (x%4 === 0));
+    });
+
+    var odd = _.reject(array, function(x){
+      return(x%2 === 0);
+    });
+
+    console.log(array);
+    console.log(next);
+    console.log(odd);
+
+
+  }
+
+
+
+
+
+})();
