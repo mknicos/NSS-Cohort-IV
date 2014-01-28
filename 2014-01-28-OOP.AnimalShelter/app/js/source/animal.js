e67670e9c77533df967e6ca1469ffff5fbed347d
Matt Knicos <mknicos@gmail.com>
2014-01-28 14:33:09 -0600

    animal.js and animal factory files linked

---
+++ 2014-01-28-OOP.AnimalShelter/app/js/source/animal.js
@@ -0,0 +1,19 @@
+/* jshint unused: false */
+
+(function(){
+
+
+  'use strict';
+
+  window.Animal = function(name, age, gender, photos, description, color, species){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.photos = photos;
+    this.description = description;
+    this.color = color;
+    this.species = species;
+  };
+
+
+})();
--- 2014-01-28-OOP.AnimalShelter/app/js/source/main.js
+++ 2014-01-28-OOP.AnimalShelter/app/js/source/main.js
@@ -1,11 +1,57 @@
+/* global Animal: false */
+
 (function(){
 
   'use strict';
 
   $(document).ready(initialize);
 
+  var radio;
+  var animals = [];
+
   function initialize(){
+    $('#addPhoto').click(clickAddPhoto);
+    $('#addAnimal').click(clickAddAnimal);
+    $('form input').on('change', function() {
+      radio = $('input[name="gender"]:checked', 'form').val();
+    });
+  }
+
+  function clickAddPhoto(){
+      var url = $('#photoURL').val();
+      var $div = $('<div>');
+      $div.css('background-image', 'url(' + url + ')');
+      $('#photocontain').prepend($div);
+      $('#photoURL').val('');
+      $('#photoURL').focus();
+      event.preventDefault();
+    }
+
+  function clickAddAnimal(){
+    var name = $('#name').val();
+    var species = $('#species').val();
+    var age = $('#age').val() * 1;
+    var color = $('#color').val();
+    var description = $('#description').val();
+    var photos = getAnimalPhotos();
+    var gender = radio;
+
+    debugger;
+    var animal = new Animal(name, age, gender, photos, description, color, species);
+    animals.push(animal);
+
+  }
+
+  function getAnimalPhotos(){
+    var urls = [];
+    for(var i = 0; i < $('#photocontain div').length; i++){
+      var $div = $('#photocontain div')[i];
+      var imageURL = $div.style.backgroundImage;
+      urls.push(imageURL);
+    }
+    console.log(urls);
+
+    event.preventDefault();
   }
 
 })();
-
