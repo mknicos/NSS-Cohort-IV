2e8104f3b1386ef189129acc596d064369902ed3
Matt Knicos <mknicos@gmail.com>
2014-02-12 00:40:58 -0600

    path repair

--- 2014-02-11-Node-Mongo-Exercises/client/app/js/source/main.js
+++ 2014-02-11-Node-Mongo-Exercises/client/app/js/source/main.js
@@ -28,18 +28,15 @@
   }
 
   function exerciseCreated(){
-
   }
 
   function getExercises(){
     var url = window.location.origin.replace(/3000/, '4000');
-    url += '/exercises/swim';
-    console.log(url);
+    url += '/exercises';
     $.getJSON(url, displayExercises);
   }
 
   function displayExercises(data){
-    console.log(data);
     for(var i = 0; i < data.exercises.length; i++){
       var $name = $('<td>');
       var $time = $('<td>');
--- 2014-02-11-Node-Mongo-Exercises/server/app/routes/exercises.js
+++ 2014-02-11-Node-Mongo-Exercises/server/app/routes/exercises.js
@@ -1,43 +1,38 @@
 'use strict';
 var MongoClient = require('mongodb').MongoClient;
 var Exercise = require('../models/exercise');
-exports.create = function(req, res){
-  console.log('POINT 1');
 
-  MongoClient.connect('mongodb://localhost/exercises', function(err, db) {
+exports.create = function(req, res){
+  MongoClient.connect('mongodb://localhost/gym', function(err, db) {
     if(err){ throw err;}
-    console.log('POINT 2');
 
     var exercise = new Exercise(req.body.name, req.body.time, req.body.calories, req.body.date);
 
     db.collection('exercises').insert(exercise, function(err, records){
       if(err){ throw err;}
-      console.log('POINT 3');
       res.send(records[0]);
     });
   });
 };
 
 exports.index = function(req, res){
-  MongoClient.connect('mongodb://localhost/exercises', function (err, db){
+  MongoClient.connect('mongodb://localhost/gym', function (err, db){
     if(err){ throw err;}
-    console.log('POINT 4');
 
-    var query ={};
-    query.name = req.params.name;
-
-    db.collection('exercises').find(query).toArray(function(err, exercises){
+    db.collection('exercises').find().toArray(function(err, exercises){
       res.send({exercises:exercises});
     });
   });
 };
 
 exports.queryName = function(req, res){
-  MongoClient.connect('mongodb://localhost/exercises', function (err, db){
+  MongoClient.connect('mongodb://localhost/gym', function (err, db){
     if(err){ throw err;}
-    console.log('POINT 5');
 
-    db.collection('exercises').find({name:'run'}).toArray(function(err, exercises){
+    var query = {};
+    query.name = req.params.name;
+
+    db.collection('exercises').find(query).toArray(function(err, exercises){
       res.send({exercises:exercises});
     });
   });
