fb350047be6dde6b6067b7daa296a9d1fa692c36
Matt Knicos <mknicos@gmail.com>
2014-02-13 09:59:57 -0600

    delete button added

--- 2014-02-12-Node-Mongo.Movies/client/app/js/source/main.js
+++ 2014-02-12-Node-Mongo.Movies/client/app/js/source/main.js
@@ -36,25 +36,27 @@
     for(var i = 0; i < data.movies.length; i++){
       var $div = $('<div>');
       var $div2 = $('<div>');
-      var $span1 = $('<span>');
+      var $span1 = $('<span>'); //Title
       var $span2 = $('<span>');
       var $span3 = $('<span>');
       var $span4 = $('<span>');
       var $span5 = $('<span>');
       var $span6 = $('<span>');
       var $span7 = $('<span>');
+      var $span8 = $('<span>'); //Delete Button
       $span1.text(mov[i].name).addClass('title');
-      $span2.text(mov[i].rating);
-      $span3.text(mov[i].runTime);
-      $span4.text(mov[i].year);
-      $span5.text(mov[i].studio);
-      $span6.text(mov[i].actors.join(' & '));
-      $span7.text(mov[i].director);
+      $span2.text(mov[i].rating).addClass('span');
+      $span3.text(mov[i].runTime).addClass('span');
+      $span4.text(mov[i].year).addClass('span');
+      $span5.text(mov[i].studio).addClass('span');
+      $span6.text(mov[i].actors.join(' & ')).addClass('span');
+      $span7.text(mov[i].director).addClass('span');
+      $span8.text('X').addClass('delete');
       $div2.append($span2, $span3, $span4, $span5, $span6, $span7);
       $div.css('background-image', 'url(' + mov[i].poster + ')');
       $div.addClass('posts');
       $div2.addClass('footer');
-      $div.append($span1, $div2);
+      $div.append($span1, $span8, $div2);
       $('#post').append($div);
     }
 
